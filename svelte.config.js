131e98432d176fefdf7ac3f0807367d900811c2c
omniflare <namikazesatyam@gmail.com>
2024-12-16 17:37:01 +0530

    feat : update for the final deplloyment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-
+ 
 export default {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x'
-    }),
-    paths: {
-      base: process.env.BASE_PATH || ''
-    }
-  }
+    adapter: adapter(),
+  },
 };
